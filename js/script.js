2d471edc52ace79e4e1162434cd805483ee4d180
Amanda <amanda770328@gmail.com>
2024-05-03 11:47:12 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 'auto',
-    spaceBetween: 250,
-    breakpoints: {
-        1440: {
-            spaceBetween: 230,
-        },
-        992: {
-            spaceBetween: 200,
-        },
-        768: {
-            spaceBetween: 120,
-        },
-        480: {
-            spaceBetween: 110,
-        },
-    },   
+    spaceBetween: 250,  
     centeredSlides: true,
     grabCursor: true,
     loop: true,
@@ -41,3 +27,53 @@ scrollNav();
 $(document).ready(function () {
 	new WOW().init();
 });
+
+// Multiple slides per carousel
+const myCarouselTwo = document.querySelector('#recipeCarousel')
+let items = document.querySelectorAll('#recipeCarousel.carousel .carousel-item')
+
+items.forEach((el) => {
+const minPerSlide = 4
+let next = el.nextElementSibling
+for (var i=1; i<minPerSlide; i++) {
+    if (!next) {
+        // wrap carousel by using first child
+        next = items[0]
+    }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+}
+}) // end multiple slides per carousel
+
+// Get either the existing Bootstrap Carousel instance or create a new one
+const carouselTwo = bootstrap.Carousel.getOrCreateInstance(myCarouselTwo);
+
+// Grab your radio indicators, the previous and next buttons/arrows
+const indicatorsAll = document.querySelectorAll('.carousel-indicators');
+const prevCarouselBtn = document.querySelector('.carousel-control-prev');
+const nextCarouselBtn = document.querySelector('.carousel-control-next');
+
+// Add event listeners to your indicators, previous and next buttons/arrows
+indicatorsAll.forEach( IndicatorBtn => IndicatorBtn.addEventListener("click", goToIndicatorCarousels));
+prevCarouselBtn.addEventListener('click', prevCarousels);
+nextCarouselBtn.addEventListener('click', nextCarousels);
+
+// Call the Bootstrap 'to' function for both Carousels to go to the indicator page that was clicked
+function goToIndicatorCarousels (e){
+let clickedIndicator = e.target.attributes[1].value;
+carouselOne.to(clickedIndicator);
+carouselTwo.to(clickedIndicator);
+}
+
+// Call the Bootstrap 'prev' function for both Carousels to go to the previous page
+function prevCarousels(){
+carouselOne.prev();
+carouselTwo.prev();
+}
+
+// Call the Bootstrap 'next' function for both Carousels to go to the next page
+function nextCarousels(){
+carouselOne.next();
+carouselTwo.next();
+}
